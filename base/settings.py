6aff4ea94e4935de6ab0da939150ca2d293acc7c
yasser sabsibo <yassersabsibo@gmail.com>
2022-06-23 16:20:14 +0100

    setup mysql

--- base/settings.py
+++ base/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'ecommerce',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306'
     }
 }
 
