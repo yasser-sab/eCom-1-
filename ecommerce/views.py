0585f5868a2b2a0c8a50af16b3a9fa5edb823e02
yasser sabsibo <yassersabsibo@gmail.com>
2022-06-24 16:33:49 +0100

    intergrate dynamic html for categorie page

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -7,3 +7,13 @@ from django.http import HttpResponse
 class Home(View):
     def get(self,response):
         return render(response,'index.html')
+
+class Categorie(View):
+    def get(self,response,cat):
+        title = {
+            "men": "Men\'s",
+            "women": "Women's",
+            "kid": "Kid's",
+            "accessories": "Accessories",
+        }
+        return render(response,'categorie.html',{'categorie':cat,'title':title[cat]})
