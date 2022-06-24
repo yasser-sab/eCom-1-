13a7616cb838afa0c5e9d72abc6008dab4de239c
Anas <ayoubanas56@gmail.com>
2022-06-24 18:15:00 +0100

    creating product, contact, about pages

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.Home.as_view(),name='home'),
+    path('products/', views.Products),
+    path('single_product/', views.SingleProduct),
+    path('contact_us/', views.Contact),
+    path('about_us/', views.About),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -7,3 +7,16 @@ from django.http import HttpResponse
 class Home(View):
     def get(self,response):
         return render(response,'index.html')
+
+def Products(request):
+    return render(request, 'products.html')
+
+def SingleProduct(request):
+    return render(request, 'single-product.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
+
+def About(request):
+    return render(request, 'about.html')
+
