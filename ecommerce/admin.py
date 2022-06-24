9dc587a5b6c635a0b28a1a2ba8c5e033ebf20cc1
jouiad <ibtissamjouiad2001@gmail.com>
2022-06-24 20:21:32 +0100

    create tables for database

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from .models import Products
+from .models import Tags
+
+
+from .models import Client
+from .models import Categories
+from .models import Commande
+
+
+
+
+
+
+admin.site.register(Products)
+
+admin.site.register(Tags)
+
+
+
+admin.site.register(Client)
+admin.site.register(Categories)
+admin.site.register(Commande)
+
+
+
+
+
+
 # Register your models here.
